10d586bc410c74434f4025ee05e8402919011f01
nelson <asum.nelson@gmail.com>
2024-12-22 12:23:13 +0800

    removed deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building app.."
-npm run build
-
-echo "Deploying React app to server.."
-scp -i ~/.ssh/id_ed25519 -r ./build/* ubuntu@13.215.89.97:/var/www/fitness-lifestyle-app/fitness-tracker
-
-echo "Deployment Complete!"
